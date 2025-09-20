2261b38aa851f5b5788eb406e3bc0acd41ca9595
sumit_chachadi <sumit.chachadi@airbnb.com>
2025-09-20 23:48:10 +0530

    include vercel insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/layout/Hero'
 import React from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <SpeedInsights />
     </div>
   )
 }
